1eb617d5d98a3c1a0a87f8bfba12a462a9845702
Jonathan Meyer <jlmeyer@gmail.com>
2013-07-15 15:35:18 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DonorPerformance::Application.config.secret_token = 'bbe61f8cf8599179bd7ac706f71c7d7943aaf6c64d380c44ad49bb68b86d321df53f0f238c493dae26fc64da9af63da2687c6d691306c7d2c1cdd1a0cab2173e'
